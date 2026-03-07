90cc606629a6265623ace07fb4eb99cb5d3be34c
Aukvary <al3xpop123@gmail.com>
2026-03-07 22:17:00 +0700

    add nuklear api

--- Editor/main_editor.c
+++ Editor/main_editor.c
@@ -1,119 +1,41 @@
-/* nuklear - 1.32.0 - public domain */
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdarg.h>
-#include <string.h>
-#include <math.h>
-#include <assert.h>
-#include <math.h>
-#include <limits.h>
-#include <time.h>
+#include "Ecs/RegisterHandler.h"
 
 #include "raylib.h"
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//#define NK_INCLUDE_FIXED_TYPES
-//#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-//#define NK_INCLUDE_DEFAULT_ALLOCATOR
-//#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-//#define NK_INCLUDE_FONT_BAKING
-//#define NK_INCLUDE_DEFAULT_FONT
-#define RAYLIB_NUKLEAR_IMPLEMENTATION
 #define RAYLIB_NUKLEAR_INCLUDE_DEFAULT_FONT
 #include "raylib-nuklear.h"
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
-
-/* ===============================================================
- *
- *                          EXAMPLE
- *
- * ===============================================================*/
-/* This are some code examples to provide a small overview of what can be
- * done with this library. To try out an example uncomment the defines */
-#define INCLUDE_ALL
-/*#define INCLUDE_STYLE */
-/*#define INCLUDE_CALCULATOR */
-/*#define INCLUDE_CANVAS */
-/*#define INCLUDE_OVERVIEW */
-/*#define INCLUDE_NODE_EDITOR */
-
-#ifdef INCLUDE_ALL
-  //#define INCLUDE_STYLE
-  #define INCLUDE_CALCULATOR
-  #define INCLUDE_CANVAS
-  #define INCLUDE_OVERVIEW
-  //#define INCLUDE_NODE_EDITOR
-#endif
-
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
 struct nk_context *ctx;
 struct nk_colorf bg;
 
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void);     // Update and Draw one frame
+void UpdateDrawFrame(void);
 
-//----------------------------------------------------------------------------------
-// Main Entry Point
-//----------------------------------------------------------------------------------
 int main(void) {
-        // Initialization
-    //--------------------------------------------------------------------------------------
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "[raylib-nuklear] demo");
     SetTargetFPS(60);
 
     /* GUI */
-    const int fontSize = 13;
+    const int fontSize = 18;
     Font font = LoadFontFromNuklear(fontSize);
     ctx = InitNuklearEx(font, fontSize);
     bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
 
-    /* style.c */
-    #ifdef INCLUDE_STYLE
-    /*set_style(ctx, THEME_WHITE);*/
-    /*set_style(ctx, THEME_RED);*/
-    /*set_style(ctx, THEME_BLUE);*/
-    /*set_style(ctx, THEME_DARK);*/
-    #endif
-
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         UpdateDrawFrame();
     }
-#endif
-    //--------------------------------------------------------------------------------------
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadNuklear(ctx);     // Unload the Nuklear GUI
+    UnloadNuklear(ctx);
     CloseWindow();
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
 
 
 void UpdateDrawFrame(void) {
-    /* Input */
     UpdateNuklear(ctx);
 
-    /* GUI */
     if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
         NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
         NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
@@ -123,8 +45,22 @@ void UpdateDrawFrame(void) {
         static int property = 20;
 
         nk_layout_row_static(ctx, 30, 80, 1);
-        if (nk_button_label(ctx, "button"))
-            TraceLog(LOG_INFO, "button pressed!");
+
+        u16 size;
+        const DtComponentData** data = dt_component_get_all(&size);
+
+        for (int i = 0; i < size; i++) {
+            if (nk_button_label(ctx, data[i]->name)) {
+
+            }
+        }
+
+        if (nk_button_label(ctx, "button")) {
+            for (int i = 0; i < size; i++) {
+                printf("%s\n", data[i]->name);
+            }
+        }
+
         nk_layout_row_dynamic(ctx, 30, 2);
         if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
         if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
@@ -147,7 +83,6 @@ void UpdateDrawFrame(void) {
     }
     nk_end(ctx);
 
-    /* Draw */
     BeginDrawing();
     {
         ClearBackground(ColorFromNuklearF(bg));
